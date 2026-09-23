a99faaa9c141e4e10d46e0fb8be0544f0f20e94f
kid <66070005@kmitl.ac.th>
2026-09-23 13:35:36 +0700

    cell05 ex01

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,8 @@
+original = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original:
+    new_array.append(number + 2)
+
+print("Original array: " + str(original))
+print("New array: " + str(new_array))
