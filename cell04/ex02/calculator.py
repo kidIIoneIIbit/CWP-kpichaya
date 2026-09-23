61f9f3681e9e33c70c264c032bcc704e330113f5
kid <66070005@kmitl.ac.th>
2026-09-23 07:17:37 +0700

    cell04 ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+first = int(input("Give me the first number: "))
+second = int(input("Give me the second number: "))
+
+print("Thank you!")
+print(str(first) + " + " + str(second) + " = " + str(first + second))
+print(str(first) + " - " + str(second) + " = " + str(first - second))
+print(str(first) + " / " + str(second) + " = " + str(first / second))
+print(str(first) + " * " + str(second) + " = " + str(first * second))
